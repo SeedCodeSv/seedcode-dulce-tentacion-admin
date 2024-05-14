1afd98864a01b752fbbde5e1cf24dc4b54efbac0
rafael vides <rafaelvides22001@gmail.com>
2024-05-14 15:11:44 -0600

    credito_address

---
+++ src/plugins/dexie/services/credito_address.service.ts
@@ -0,0 +1,6 @@
+import { db } from "../db"
+import { AddressCredito } from "../entities/address_credito_fiscal"
+
+export const save_address_credito = async (address: AddressCredito) => {
+    const id = await db.direccion
+}
