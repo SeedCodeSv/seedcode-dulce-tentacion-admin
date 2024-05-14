6b60a30ed5ac66f0918e1ee0cf454a3a4d096e31
ROBER-SALAMA <robeetosalama12@gmail.com>
2024-05-14 16:10:26 -0600

    get configuration

--- src/store/perzonalitation.store.ts
+++ src/store/perzonalitation.store.ts
@@ -1,13 +1,13 @@
 import { create } from "zustand";
 import { IConfigurationStore } from "./types/perzonalitation.types.store";
 import { IGetConfiguration } from "../types/configuration.types";
-import { create_configuration } from "../services/configuration.service";
+import { create_configuration, get_by_transmitter } from "../services/configuration.service";
 import { toast } from "sonner";
 
 export const useConfigurationStore = create<IConfigurationStore>(
   (set, get) => ({
     personalization: [],
-
+    get_personalization: [],
     OnCreateConfiguration: (payload: IGetConfiguration) => {
       create_configuration(payload)
         .then(() => {
@@ -18,5 +18,15 @@ export const useConfigurationStore = create<IConfigurationStore>(
           toast.error("Ocurrió un error al crear");
         });
     },
+
+      async GetConfigurationByTransmitter(id:number ) {
+      await get_by_transmitter(id)
+      .then(({ data }) => {
+        set({ personalization: data.personalization });
+      }).catch(() => {
+        set({ personalization: [] });
+        toast.error("Ocurrió un error al obtener la información")
+      })
+  }
   })
 );
