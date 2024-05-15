7778dff046d2c80148dd8adea26c5efed4abe190
manuelhm98 <marquezmanuel1516@gmail.com>
2024-05-14 17:15:14 -0700

    ok all 3

--- src/store/report_contigence.store.ts
+++ src/store/report_contigence.store.ts
@@ -1,19 +1,52 @@
 import { create } from "zustand";
 import { ISalesContigenceStore } from "../types/report_contigence";
-import { get_contigence_not_report, get_contigence_report } from "../services/report_contigence.service";
+import { IPagination } from "../types/global.types";
+import {
+  get_contigence_not_report,
+  get_contigence_report,
+} from "../services/report_contigence.service";
 
 export const useReportContigenceStore = create<ISalesContigenceStore>(
   (set) => ({
     sales: [],
     saless: [],
-    OnGetSalesContigence: async (id: number, page: number, limit: number) => {
-      const { data } = await get_contigence_report(id, page, limit);
-      set({ sales: data.sales });
+    pagination_sales: {} as IPagination,
+    pagination_saless: {} as IPagination,
+    OnGetSalesContigence: async (id: number, page: number, limit: number , startDate: string, endDate: string) => {
+      const { data } = await get_contigence_report(id, page, limit, startDate, endDate);
+      set({
+        sales: data.sales,
+        pagination_sales: {
+          total: data.total,
+          totalPag: data.totalPag,
+          currentPag: data.currentPag,
+          nextPag: data.nextPag,
+          prevPag: data.prevPag,
+          status: data.status,
+          ok: data.ok,
+        },
+      });
     },
-    OnGetSalesNotContigence: async (id: number, page: number, limit: number) => {
-      const { data } = await get_contigence_not_report(id, page, limit);
-      set({ saless: data.saless });
+    OnGetSalesNotContigence: async (
+      id: number,
+      page: number,
+      limit: number,
+      startDate: string,
+      endDate: string
+    ) => {
+      const { data } = await get_contigence_not_report(id, page, limit , startDate, endDate);
+      set({
+        saless: data.saless,
+        pagination_saless: {
+          total: data.total,
+          totalPag: data.totalPag,
+          currentPag: data.currentPag,
+          nextPag: data.nextPag,
+          prevPag: data.prevPag,
+          status: data.status,
+          ok: data.ok,
+        },
+      });
     },
   })
 );
-
