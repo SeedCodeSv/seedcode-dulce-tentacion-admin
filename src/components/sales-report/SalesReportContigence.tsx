3b985f8407b3ec66c6b92d5a8e148afb375a2c6a
danniel-contreras <contrerasdannniel@gmail.com>
2024-05-15 09:36:50 -0600

    DRY method in sales

--- src/components/sales-report/SalesReportContigence.tsx
+++ src/components/sales-report/SalesReportContigence.tsx
@@ -13,7 +13,7 @@ import Terminal, {
   TerminalInput,
   TerminalOutput,
 } from "react-terminal-ui";
-import { fechaActualString } from "../../utils/dates";
+import { fechaActualString, formatDate } from "../../utils/dates";
 import Pagination from "../global/Pagination";
 import { Paginator } from "primereact/paginator";
 import { useLogsStore } from "../../store/logs.store";
@@ -53,12 +53,13 @@ function SalesReportContigence() {
       }
     }
   }, [branchId]);
-  const [dateInitial, setDateInitial] = useState("");
-  const [dateEnd, setDateEnd] = useState("");
+  const [dateInitial, setDateInitial] = useState(formatDate());
+  const [dateEnd, setDateEnd] = useState(formatDate());
   const searchSalesContigence = () => {
     OnGetSalesContigence(branchId, 1, 5, dateInitial, dateEnd);
   };
   const searchSalesNotContigence = () => {
+    searchSalesContigence()
     OnGetSalesNotContigence(branchId, 1, 5, dateInitial, dateEnd);
   };
   const { theme } = useContext(ThemeContext);
@@ -119,257 +120,221 @@ function SalesReportContigence() {
 
   return (
     <>
-      {isActive === true ? (
-        <div className="w-full h-full p-5 bg-gray-100 dark:bg-gray-800">
-          <div className="w-full h-full p-5 overflow-y-auto bg-white shadow rounded-xl dark:bg-transparent">
-            <div className="w-full grid grid-cols-3 gap-5 mb-5">
-              <Input
-                onChange={(e) => setDateInitial(e.target.value)}
-                placeholder="Buscar por nombre..."
-                size="lg"
-                type="date"
-                variant="bordered"
-                label="Fecha inicial"
-                labelPlacement="outside"
-                classNames={{
-                  input: "dark:text-white dark:border-gray-600",
-                  label: "text-sm font-semibold dark:text-white",
-                }}
-              />
-              <Input
-                onChange={(e) => setDateEnd(e.target.value)}
-                placeholder="Buscar por nombre..."
-                size="lg"
-                variant="bordered"
-                label="Fecha final"
-                type="date"
-                labelPlacement="outside"
-                classNames={{
-                  input: "dark:text-white dark:border-gray-600",
-                  label: "text-sm font-semibold dark:text-white",
-                }}
-              />
-              <Button
-                onClick={searchSalesNotContigence}
-                className="bg-gray-900 text-white mt-7 dark:bg-gray-700 dark:text-gray-200"
-              >
-                Buscar
-              </Button>
-            </div>
+      <div className="w-full h-full p-5 bg-gray-100 dark:bg-gray-800">
+        <div className="w-full h-full p-5 overflow-y-auto bg-white shadow rounded-xl dark:bg-transparent">
+          <div className="w-full grid grid-cols-3 gap-5 mb-5">
+            <Input
+              onChange={(e) => setDateInitial(e.target.value)}
+              value={dateInitial}
+              defaultValue={formatDate()}
+              placeholder="Buscar por nombre..."
+              size="lg"
+              type="date"
+              variant="bordered"
+              label="Fecha inicial"
+              labelPlacement="outside"
+              classNames={{
+                input: "dark:text-white dark:border-gray-600",
+                label: "text-sm font-semibold dark:text-white",
+              }}
+            />
+            <Input
+              onChange={(e) => setDateEnd(e.target.value)}
+              value={dateEnd}
+              placeholder="Buscar por nombre..."
+              size="lg"
+              variant="bordered"
+              label="Fecha final"
+              type="date"
+              labelPlacement="outside"
+              classNames={{
+                input: "dark:text-white dark:border-gray-600",
+                label: "text-sm font-semibold dark:text-white",
+              }}
+            />
+            <Button
+              onClick={searchSalesNotContigence}
+              className="bg-gray-900 text-white mt-7 dark:bg-gray-700 dark:text-gray-200"
+            >
+              Buscar
+            </Button>
+          </div>
 
-            <div className="flex overflow-hidden justify-end  mb-2 mr-3">
-              <Switch onChange={() => setIsActive(!isActive)} defaultSelected>
-                {isActive ? "No Contigencia" : "Contigencia"}
-              </Switch>
-            </div>
-            <DataTable
-              className="shadow"
-              emptyMessage="No se encontraron resultados"
-              value={saless}
-              tableStyle={{ minWidth: "50rem" }}
-            >
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={{ ...style, borderTopLeftRadius: "10px" }}
-                field="id"
-                header="No."
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="fecEmi"
-                header="Fecha de Emisión"
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="horEmi"
-                header="Hora de Emisión"
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="subTotal"
-                header="Subtotal"
-                body={(rowData) => formatCurrency(Number(rowData.subTotal))}
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                header="Total IVA"
-                body={(rowData) => formatCurrency(Number(rowData.totalIva))}
-              />
-            </DataTable>
-            {pagination_saless.totalPag > 1 && (
-              <>
-                <div className="hidden w-full mt-5 md:flex">
-                  <Pagination
-                    previousPage={pagination_saless.prevPag}
-                    nextPage={pagination_saless.nextPag}
-                    currentPage={pagination_saless.currentPag}
-                    totalPages={pagination_saless.totalPag}
-                    onPageChange={(pageNumber: number) =>
-                      OnGetSalesNotContigence(
-                        branchId,
-                        pageNumber,
-                        5,
-                        fechaActualString,
-                        fechaActualString
-                      )
-                    }
-                  />
-                </div>
-                <div className="flex w-full mt-5 md:hidden">
-                  <Paginator
-                    className="flex justify-between w-full"
-                    first={pagination_saless.currentPag}
-                    totalRecords={pagination_saless.total}
-                    template={{
-                      layout: "PrevPageLink CurrentPageReport NextPageLink",
-                    }}
-                    currentPageReportTemplate="{currentPage} de {totalPages}"
-                  />
-                </div>
-              </>
-            )}
+          <div className="flex overflow-hidden justify-end  mb-2 mr-3">
+            <Switch onChange={() => setIsActive(!isActive)} defaultSelected>
+              {isActive ? "No Contigencia" : "Contigencia"}
+            </Switch>
           </div>
-        </div>
-      ) : (
-        <div className="w-full h-full p-5 bg-gray-100 dark:bg-gray-800">
-          <div className="w-full h-full p-5 overflow-y-auto bg-white shadow rounded-xl dark:bg-transparent">
-            <div className="w-full grid grid-cols-3 gap-5 mb-5">
-              <Input
-                onChange={(e) => setDateInitial(e.target.value)}
-                placeholder="Buscar por nombre..."
-                size="lg"
-                type="date"
-                variant="bordered"
-                label="Fecha inicial"
-                labelPlacement="outside"
-                classNames={{
-                  input: "dark:text-white dark:border-gray-600",
-                  label: "text-sm font-semibold dark:text-white",
-                }}
-              />
-              <Input
-                onChange={(e) => setDateEnd(e.target.value)}
-                placeholder="Buscar por nombre..."
-                size="lg"
-                variant="bordered"
-                label="Fecha final"
-                type="date"
-                labelPlacement="outside"
-                classNames={{
-                  input: "dark:text-white dark:border-gray-600",
-                  label: "text-sm font-semibold dark:text-white",
-                }}
-              />
-              <Button
-                onClick={searchSalesContigence}
-                className="bg-gray-900 text-white mt-7 dark:bg-gray-700 dark:text-gray-200"
+          {isActive === true ? (
+            <>
+              <DataTable
+                className="shadow"
+                emptyMessage="No se encontraron resultados"
+                value={saless}
+                tableStyle={{ minWidth: "50rem" }}
               >
-                Buscar
-              </Button>
-            </div>
-            <div className="flex overflow-hidden justify-end  mb-2 mr-3">
-              <Switch onChange={() => setIsActive(!isActive)} defaultSelected>
-                {isActive ? "No Contigencia" : "Contigencia"}
-              </Switch>
-            </div>
-            <DataTable
-              className="shadow"
-              emptyMessage="No se encontraron resultados"
-              value={sales}
-              tableStyle={{ minWidth: "50rem" }}
-            >
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={{ ...style, borderTopLeftRadius: "10px" }}
-                field="id"
-                header="No."
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="fecEmi"
-                header="Fecha de Emisión"
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="horEmi"
-                header="Hora de Emisión"
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="subTotal"
-                header="Subtotal"
-                body={(rowData) => formatCurrency(Number(rowData.subTotal))}
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                // field="totalIva"
-                header="Total IVA"
-                body={(rowData) => formatCurrency(Number(rowData.totalIva))}
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                // field="totalIva"
-                header="Acciones"
-                body={(rowData) => (
-                  <div>
-                    <Button
-                      style={global_styles().dangerStyles}
-                      size="lg"
-                      isIconOnly
-                      onClick={() => {
-                        handleSelectLogs(rowData.codigoGeneracion);
-                      }}
-                    >
-                      <SquareChevronRight />
-                    </Button>
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={{ ...style, borderTopLeftRadius: "10px" }}
+                  field="id"
+                  header="No."
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  field="fecEmi"
+                  header="Fecha de Emisión"
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  field="horEmi"
+                  header="Hora de Emisión"
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  field="subTotal"
+                  header="Subtotal"
+                  body={(rowData) => formatCurrency(Number(rowData.subTotal))}
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  header="Total IVA"
+                  body={(rowData) => formatCurrency(Number(rowData.totalIva))}
+                />
+              </DataTable>
+              {pagination_saless.totalPag > 1 && (
+                <>
+                  <div className="hidden w-full mt-5 md:flex">
+                    <Pagination
+                      previousPage={pagination_saless.prevPag}
+                      nextPage={pagination_saless.nextPag}
+                      currentPage={pagination_saless.currentPag}
+                      totalPages={pagination_saless.totalPag}
+                      onPageChange={(pageNumber: number) =>
+                        OnGetSalesNotContigence(
+                          branchId,
+                          pageNumber,
+                          5,
+                          fechaActualString,
+                          fechaActualString
+                        )
+                      }
+                    />
                   </div>
-                )}
-              />
-            </DataTable>
-            {pagination_sales.totalPag > 1 && (
-              <>
-                <div className="hidden w-full mt-5 md:flex">
-                  <Pagination
-                    previousPage={pagination_sales.prevPag}
-                    nextPage={pagination_sales.nextPag}
-                    currentPage={pagination_sales.currentPag}
-                    totalPages={pagination_sales.totalPag}
-                    onPageChange={(pageNumber: number) =>
-                      OnGetSalesContigence(
-                        branchId,
-                        pageNumber,
-                        5,
-                        fechaActualString,
-                        fechaActualString
-                      )
-                    }
-                  />
-                </div>
-                <div className="flex w-full mt-5 md:hidden">
-                  <Paginator
-                    className="flex justify-between w-full"
-                    first={pagination_sales.currentPag}
-                    totalRecords={pagination_sales.total}
-                    template={{
-                      layout: "PrevPageLink CurrentPageReport NextPageLink",
-                    }}
-                    currentPageReportTemplate="{currentPage} de {totalPages}"
-                  />
-                </div>
-              </>
-            )}
-          </div>
+                  <div className="flex w-full mt-5 md:hidden">
+                    <Paginator
+                      className="flex justify-between w-full"
+                      first={pagination_saless.currentPag}
+                      totalRecords={pagination_saless.total}
+                      template={{
+                        layout: "PrevPageLink CurrentPageReport NextPageLink",
+                      }}
+                      currentPageReportTemplate="{currentPage} de {totalPages}"
+                    />
+                  </div>
+                </>
+              )}
+            </>
+          ) : (
+            <>
+              <DataTable
+                className="shadow"
+                emptyMessage="No se encontraron resultados"
+                value={sales}
+                tableStyle={{ minWidth: "50rem" }}
+              >
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={{ ...style, borderTopLeftRadius: "10px" }}
+                  field="id"
+                  header="No."
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  field="fecEmi"
+                  header="Fecha de Emisión"
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  field="horEmi"
+                  header="Hora de Emisión"
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  field="subTotal"
+                  header="Subtotal"
+                  body={(rowData) => formatCurrency(Number(rowData.subTotal))}
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  // field="totalIva"
+                  header="Total IVA"
+                  body={(rowData) => formatCurrency(Number(rowData.totalIva))}
+                />
+                <Column
+                  headerClassName="text-sm font-semibold"
+                  headerStyle={style}
+                  // field="totalIva"
+                  header="Acciones"
+                  body={(rowData) => (
+                    <div>
+                      <Button
+                        style={global_styles().dangerStyles}
+                        size="lg"
+                        isIconOnly
+                        onClick={() => {
+                          handleSelectLogs(rowData.codigoGeneracion);
+                        }}
+                      >
+                        <SquareChevronRight />
+                      </Button>
+                    </div>
+                  )}
+                />
+              </DataTable>
+              {pagination_sales.totalPag > 1 && (
+                <>
+                  <div className="hidden w-full mt-5 md:flex">
+                    <Pagination
+                      previousPage={pagination_sales.prevPag}
+                      nextPage={pagination_sales.nextPag}
+                      currentPage={pagination_sales.currentPag}
+                      totalPages={pagination_sales.totalPag}
+                      onPageChange={(pageNumber: number) =>
+                        OnGetSalesContigence(
+                          branchId,
+                          pageNumber,
+                          5,
+                          dateInitial,
+                          dateEnd
+                        )
+                      }
+                    />
+                  </div>
+                  <div className="flex w-full mt-5 md:hidden">
+                    <Paginator
+                      className="flex justify-between w-full"
+                      first={pagination_sales.currentPag}
+                      totalRecords={pagination_sales.total}
+                      template={{
+                        layout: "PrevPageLink CurrentPageReport NextPageLink",
+                      }}
+                      currentPageReportTemplate="{currentPage} de {totalPages}"
+                    />
+                  </div>
+                </>
+              )}
+            </>
+          )}
         </div>
-      )}
+      </div>
       <ModalGlobal
         title=""
         isOpen={modalContingencia.isOpen}
