cbf8c2dd404f9ea836b6dac1b5bf49ebd94d0243
ROBER-SALAMA <robeetosalama12@gmail.com>
2024-05-20 13:51:31 -0600

    fix bug in configuration

--- src/components/configuration/ConfigurationList.tsx
+++ src/components/configuration/ConfigurationList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Card, useDisclosure } from "@nextui-org/react";
 import { useThemeStore } from "../../store/theme.store";
 import { Theme, ThemeContext } from "../../hooks/useTheme";
@@ -8,16 +8,13 @@ import ModalGlobal from "../global/ModalGlobal";
 import CreateConfiguration from "./CreateConfiguration";
 import CreateTheme from "./CreateTheme";
 import { useConfigurationStore } from "../../store/perzonalitation.store";
-import { Avatar } from "@nextui-org/react";
-import { CardHeader, CardFooter, Divider } from "@nextui-org/react";
-import DefaultImage from "../../assets/react.svg";
 import { useAuthStore } from "../../store/auth.store";
 import UpdateFile from "./UpdateFile";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import UpdateConfigurationName from "./UpdateConfigurationName";
 import { Button } from "@nextui-org/react";
-import { Image } from 'primereact/image';
+import { Image } from "primereact/image";
 
 function ConfigurationList() {
   const { getPaginatedThemes, themes } = useThemeStore();
@@ -45,7 +42,71 @@ function ConfigurationList() {
 
   return (
     <>
-      <div className="grid grid-cols-2 gap-0 bg-gray-50 dark:bg-gray-800 h-full w-full">
+      <div className="p-4 dark:bg-gray-800">
+        <div className="flex items-end justify-between gap-10 mt lg:justify-end mt-5 mr-5">
+          {personalization.length === 0 && (
+            <AddButton onClick={() => addLogo.onOpen()} />
+          )}
+          {personalization.length > 0 &&
+            personalization.map((item) => (
+              <AddButton
+                key={item.id}
+                onClick={() => {
+                  UpdateImgModal.onOpen();
+                  setLogoId(item.id || 0);
+                }}
+              />
+            ))}
+        </div>
+
+        <div className="flex justify-center p-5 bg-gray-50 dark:bg-gray-800">
+          <div className="bg-gray-50 w-full dark:bg-gray-800">
+            {personalization.length === 0 ? (
+              <span>no ay datos de logo ni nombre...</span>
+            ) : (
+              <DataTable value={personalization}>
+                <Column
+                  field="logo"
+                  header="Logo"
+                  body={(rowData) => (
+                    <Image
+                      preview
+                      src={rowData.logo}
+                      alt={rowData.name}
+                      style={{ width: "100px" }}
+                    />
+                  )}
+                />
+                <Column field="name" header="Nombre" />
+                <Column
+                  header="Actualizar Nombre"
+                  body={(rowData) => (
+                    <>
+                      <Button onClick={() => updateName.onOpen()}>
+                        Actualizar
+                      </Button>
+                      <ModalGlobal
+                        isOpen={updateName.isOpen}
+                        onClose={updateName.onClose}
+                        title="Actualizar nombre"
+                        size="w-full lg:w-[300px]"
+                      >
+                        <UpdateConfigurationName
+                          id={rowData.id || 0}
+                          reloadData={reloadData}
+                          onClose={updateName.onClose}
+                        />
+                      </ModalGlobal>
+                    </>
+                  )}
+                />
+              </DataTable>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-50 dark:bg-gray-800 h-full w-full">
         <div className="p-4 ">
           <div className="flex items-end justify-between gap-10 mt lg:justify-end mt-5 mr-5">
             <AddButton onClick={() => modalAdd.onOpen()} />
@@ -91,85 +152,6 @@ function ConfigurationList() {
                   ></span>
                 </Card>
               ))}
-            </div>
-          </div>
-        </div>
-        <div className="p-4 bg-gray-50 dark:bg-gray-800">
-          <div className="flex items-end justify-between gap-10 mt lg:justify-end mt-5 mr-5">
-            {personalization.length === 0 && (
-              <AddButton onClick={() => addLogo.onOpen()} />
-            )}
-            {personalization.length > 0 &&
-              personalization.map((item) => (
-                <AddButton
-                  key={item.id}
-                  onClick={() => {
-                    UpdateImgModal.onOpen();
-                    setLogoId(item.id || 0);
-                  }}
-                />
-              ))}
-          </div>
-
-          <div className="flex justify-center p-5 bg-gray-50 dark:bg-gray-800">
-            <div className="flex flex-wrap justify-center bg-gray-50 dark:bg-gray-800">
-              {personalization.length === 0 ? (
-                <span>no ay logo ni nombre...</span>
-                // <Card className="hover:shadow-xl hover:border border border-gray-400 hover:border-blue-400 w-72 h-56 m-4">
-                //   <CardHeader className="flex gap-3">
-                //     <div className="flex items-center justify-center w-full">
-                //       <Avatar
-                //         src={DefaultImage}
-                //         className="w-36 h-36 text-large"
-                //       />
-                //     </div>
-                //   </CardHeader>
-                //   <Divider />
-                //   <CardFooter className="flex justify-between">
-                //     <div className="w-full text-center">
-                //       <p>Seed code ERP</p>
-                //     </div>
-                //   </CardFooter>
-                // </Card>
-              ) : (
-                <DataTable value={personalization}>
-                  <Column
-                    field="logo"
-                    header="Logo"
-                    body={(rowData) => (
-                      <Image
-                        preview
-                        src={rowData.logo}
-                        alt={rowData.name}
-                        style={{ width: "100px" }}
-                      />
-                    )}
-                  />
-                  <Column field="name" header="Nombre" />
-                  <Column
-                    header="Actualizar Nombre"
-                    body={(rowData) => (
-                      <>
-                        <Button onClick={() => updateName.onOpen()}>
-                          Actualizar
-                        </Button>
-                        <ModalGlobal
-                          isOpen={updateName.isOpen}
-                          onClose={updateName.onClose}
-                          title="Actualizar nombre"
-                          size="w-full lg:w-[300px]"
-                        >
-                          <UpdateConfigurationName
-                            id={rowData.id || 0}
-                            reloadData={reloadData}
-                            onClose={updateName.onClose}
-                          />
-                        </ModalGlobal>
-                      </>
-                    )}
-                  />
-                </DataTable>
-              )}
             </div>
           </div>
         </div>
