c52fe0c9625b5b5ebb71a7eed91a511bd4b6b2fe
castrovc <vladimir_castrosv@outlook.com>
2024-05-21 18:58:16 -0600

    advance in seal for sale

--- src/components/sales-report/AddSealMH.tsx
+++ src/components/sales-report/AddSealMH.tsx
@@ -1,27 +1,162 @@
 import axios from "axios";
 import { get_json_sale } from "../../services/sales.service";
 import { IDTE } from "../../types/DTE/DTE.types";
+import { generate_factura } from "../../utils/DTE/factura";
+import { useTransmitterStore } from "../../store/transmitter.store";
+import { useEffect, useState } from "react";
+import { useBillingStore } from "../../store/facturation/billing.store";
+import { ITipoDocumento } from "../../types/DTE/tipo_documento.types";
+import { pdf } from "@react-pdf/renderer";
+import { Invoice } from "../../pages/Invoice";
+import { ambiente, API_URL, MH_QUERY } from "../../utils/constants";
+import { PutObjectCommand, PutObjectCommandInput } from "@aws-sdk/client-s3";
+import { s3Client } from "../../plugins/s3";
+import { get_token } from "../../storage/localStorage";
+import { toast } from "sonner";
+import { Sale } from "../../types/report_contigence";
+import { IFormasDePago } from "../../types/DTE/forma_de_pago.types";
 
 interface Props {
-  id: number;
-  pathPdf: string;
-  pathJson: string;
+  sale: Sale;
   sello: string;
 }
 
 export const AddSealMH = (props: Props) => {
-  get_json_sale(props.id).then((data) => {
+  useEffect(() => {
+    gettransmitter();
+    getCat02TipoDeDocumento();
+    getCat017FormasDePago();
+  }, []);
+  const { gettransmitter, transmitter } = useTransmitterStore();
+  const {
+    tipos_de_documento,
+    getCat02TipoDeDocumento,
+    getCat017FormasDePago,
+    metodos_de_pago,
+  } = useBillingStore();
+  const [loading, setLoading] = useState(false);
+  const generateURLMH = (
+    ambiente: string,
+    codegen: string,
+    fechaEmi: string
+  ) => {
+    return `${MH_QUERY}?ambiente=${ambiente}&codGen=${codegen}&fechaEmi=${fechaEmi}`;
+  };
+
+  get_json_sale(props.sale.id).then((data) => {
     axios
       .get(data.data.json)
-      .then((response) => {
-        try {
-          const jsonData: IDTE = JSON.parse(response.data);
+      .then(async (response) => {
+        const jsonData: IDTE = JSON.parse(response.data);
+        const data: any[] = [];
+        for (const item of jsonData.cuerpoDocumento) {
+          data.push({
+            quantity: item.cantidad,
+            discount: item.montoDescu,
+            percentage: (
+              (Number(item.montoDescu) / Number(item.precioUni)) *
+              100
+            ).toFixed(2),
+            total: Number(item.cantidad) * Number(item.precioUni),
+            base_price: item.precioUni,
+          });
+        }
+        jsonData.respuestaMH.selloRecibido = props.sello;
+        const correlativo = jsonData.identificacion.numeroControl.slice(-4);
+        const formaPago = metodos_de_pago.find(
+          (fp) => fp.codigo === jsonData.resumen.pagos[0].codigo
+        );
 
-          jsonData.respuestaMH.selloRecibido = props.sello;
+        const tipoDte = tipos_de_documento.find(
+          (td) => td.codigo === jsonData.identificacion.tipoDte
+        ) as ITipoDocumento;
+        const generate = generate_factura(
+          transmitter,
+          Number(correlativo),
+          tipoDte,
+          props.sale.customer,
+          data,
+          formaPago as IFormasDePago
+        );
 
-        
-        } catch (error) {
-          console.error("Error al analizar el archivo JSON:", error);
+        //json send to spapce
+        const JSON_DTE = JSON.stringify(
+          {
+            ...generate.dteJson,
+            respuestaMH: data,
+            firma: jsonData.firma,
+          },
+          null,
+          2
+        );
+        const json_blob = new Blob([JSON_DTE], {
+          type: "application/json",
+        });
+
+        const blob = await pdf(
+          <Invoice
+            MHUrl={generateURLMH(
+              ambiente,
+              generate.dteJson.identificacion.codigoGeneracion,
+              generate.dteJson.identificacion.fecEmi
+            )}
+            DTE={generate}
+            sello={props.sello}
+          />
+        ).toBlob();
+        if (json_blob && blob) {
+          const uploadParams: PutObjectCommandInput = {
+            Bucket: "seedcode-facturacion",
+            Key: props.sale.pathJson,
+            Body: json_blob,
+          };
+          const uploadParamsPDF: PutObjectCommandInput = {
+            Bucket: "seedcode-facturacion",
+            Key: props.sale.pathPdf,
+            Body: blob,
+          };
+          try {
+            s3Client
+              .send(new PutObjectCommand(uploadParamsPDF))
+              .then((response) => {
+                if (response.$metadata) {
+                  s3Client
+                    .send(new PutObjectCommand(uploadParams))
+                    .then((response) => {
+                      if (response.$metadata) {
+                        const token = get_token() ?? "";
+
+                        axios
+                          .post(
+                            API_URL + "/sales/factura-sale",
+                            {
+                              pdf: props.sale.pathPdf,
+                              dte: props.sale.pathJson,
+                              cajaId: Number(localStorage.getItem("box")),
+                              codigoEmpleado: 1,
+                              sello: true,
+                            },
+                            {
+                              headers: {
+                                Authorization: `Bearer ${token}`,
+                              },
+                            }
+                          )
+                          .then(() => {
+                            toast.success("Se completo con éxito la venta");
+                            setLoading(false);
+                          })
+                          .catch(() => {
+                            toast.error("Error al guardar la venta");
+                            setLoading(false);
+                          });
+                      }
+                    });
+                }
+              });
+          } catch (error) {
+            console.error("Error al analizar el archivo JSON:", error);
+          }
         }
       })
       .catch((error) => {
