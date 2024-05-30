d918bcbcbf4b8d9570dd6c9d3791980462e7a8b1
Marvin-Carbajal <marvinhcarbajal09@gmail.com>
2024-05-30 11:58:54 -0600

    feat: product and permission editing
    Minimum quantity field on product is required and different permission views.

--- src/components/Action_rol/ListActionRol.tsx
+++ src/components/Action_rol/ListActionRol.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonGroup, Select, SelectItem, useDisclosure } from '@nextui-org/react';
+import { Button, ButtonGroup, useDisclosure } from '@nextui-org/react';
 import { Table as ITable, CreditCard, List, CirclePlus, Trash, Pencil, Eye } from 'lucide-react';
 import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../../hooks/useTheme';
@@ -8,10 +8,14 @@ import { Column } from 'primereact/column';
 import ModalGlobal from '../global/ModalGlobal';
 import AddActionRol from './AddActionRol';
 import { useActionsRolStore } from '../../store/actions_rol.store';
-const ListActionRol = () => {
+import MobileView from './MobileView';
+interface Props {
+  actions: string[];
+}
+const ListActionRol = ({ actions }: Props) => {
   const { theme } = useContext(ThemeContext);
   const [view, setView] = useState<'table' | 'grid' | 'list'>('table');
-  const [limit, setLimit] = useState(8);
+  // const [limit, setLimit] = useState(8);
   const modalAdd = useDisclosure();
   const [idCounter] = useState(1);
   const { OnGetActionsRoleList, actions_roles_grouped } = useActionsRolStore();
@@ -23,56 +27,62 @@ const ListActionRol = () => {
     backgroundColor: theme.colors.dark,
     color: theme.colors.primary,
   };
+
   return (
     <>
       <div className="w-full h-full p-5 bg-gray-50 dark:bg-gray-800">
         <div className="w-full h-full p-5 overflow-y-auto bg-white shadow rounded-xl dark:bg-transparent">
           <div className="flex flex-col justify-between w-full gap-5 mb-5 lg:mb-10 lg:flex-row lg:gap-0">
-            <div className="flex items-end gap-3"></div>
-            <div className="flex items-end justify-between w-full gap-10 lg:justify-end">
-              <ButtonGroup>
-                <Button
-                  isIconOnly
-                  color="secondary"
-                  style={{
-                    backgroundColor: view === 'table' ? theme.colors.third : '#e5e5e5',
-                    color: view === 'table' ? theme.colors.primary : '#3e3e3e',
+            <div className="flex items-start gap-3"></div>
+
+            <div className="flex w-full">
+              <div className="items-start justify-between w-full gap-10 lg:justify-start">
+                <ButtonGroup>
+                  <Button
+                    isIconOnly
+                    color="secondary"
+                    style={{
+                      backgroundColor: view === 'table' ? theme.colors.third : '#e5e5e5',
+                      color: view === 'table' ? theme.colors.primary : '#3e3e3e',
+                    }}
+                    onClick={() => setView('table')}
+                  >
+                    <ITable />
+                  </Button>
+                  <Button
+                    isIconOnly
+                    color="default"
+                    style={{
+                      backgroundColor: view === 'grid' ? theme.colors.third : '#e5e5e5',
+                      color: view === 'grid' ? theme.colors.primary : '#3e3e3e',
+                    }}
+                    onClick={() => setView('grid')}
+                  >
+                    <CreditCard />
+                  </Button>
+                  <Button
+                    isIconOnly
+                    color="default"
+                    style={{
+                      backgroundColor: view === 'list' ? theme.colors.third : '#e5e5e5',
+                      color: view === 'list' ? theme.colors.primary : '#3e3e3e',
+                    }}
+                    onClick={() => setView('list')}
+                  >
+                    <List />
+                  </Button>
+                </ButtonGroup>
+              </div>
+              <div className="flex items-end justify-end">
+                <AddButton
+                  onClick={() => {
+                    modalAdd.onOpen();
                   }}
-                  onClick={() => setView('table')}
-                >
-                  <ITable />
-                </Button>
-                <Button
-                  isIconOnly
-                  color="default"
-                  style={{
-                    backgroundColor: view === 'grid' ? theme.colors.third : '#e5e5e5',
-                    color: view === 'grid' ? theme.colors.primary : '#3e3e3e',
-                  }}
-                  onClick={() => setView('grid')}
-                >
-                  <CreditCard />
-                </Button>
-                <Button
-                  isIconOnly
-                  color="default"
-                  style={{
-                    backgroundColor: view === 'list' ? theme.colors.third : '#e5e5e5',
-                    color: view === 'list' ? theme.colors.primary : '#3e3e3e',
-                  }}
-                  onClick={() => setView('list')}
-                >
-                  <List />
-                </Button>
-              </ButtonGroup>
-              <AddButton
-                onClick={() => {
-                  modalAdd.onOpen();
-                }}
-              />
+                />
+              </div>
             </div>
           </div>
-          <div className="flex justify-end w-full mb-5">
+          {/* <div className="flex justify-end w-full mb-5">
             <Select
               className="w-44"
               variant="bordered"
@@ -94,7 +104,12 @@ const ListActionRol = () => {
               <SelectItem key={'50'}>50</SelectItem>
               <SelectItem key={'100'}>100</SelectItem>
             </Select>
-          </div>
+          </div> */}
+
+          {(view === 'grid' || view === 'list') && (
+            <MobileView layout={view as 'grid' | 'list'} actions={actions} />
+          )}
+
           {view === 'table' && (
             <DataTable
               className="w-full shadow "
