20717e3255d574f41050e5b16d2a5cd5f0598e7c
ROBER-SALAMA <robeetosalama12@gmail.com>
2024-05-30 16:18:12 -0600

    fix: bug fixes in the theme
    add resonsive design to create thema

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,57 +8,68 @@ interface Props {
 function Button({ theme }: Props) {
   return (
     <>
-      <NextUI.Button
-        color="primary"
-        style={{
-          backgroundColor: theme.colors.danger,
-          color: theme.colors.primary,
-        }}
-        className="font-semibold"
-      >
-        Danger
-      </NextUI.Button>
-      <NextUI.Button
-        color="primary"
-        style={{
-          backgroundColor: theme.colors.dark,
-          color: theme.colors.primary,
-        }}
-        className="font-semibold"
-      >
-        Dark
-      </NextUI.Button>
-      <NextUI.Button
-        color="primary"
-        style={{
-          backgroundColor: theme.colors.secondary,
-          color: theme.colors.primary,
-        }}
-        className="font-semibold"
-      >
-        Secondary
-      </NextUI.Button>
-      <NextUI.Button
-        color="primary"
-        style={{
-          backgroundColor: theme.colors.third,
-          color: theme.colors.primary,
-        }}
-        className="font-semibold"
-      >
-        Third
-      </NextUI.Button>
-
-      <NextUI.Button
-        color="primary"
-        style={{
-          backgroundColor: theme.colors.warning,
-          color: theme.colors.primary,
-        }}
-        className="font-semibold"
-      >
-        Warning
-      </NextUI.Button>
+      <div className="grid grid-cols-3 gap-2 md:grid-cols-5 lg:grid-cols-5 w-full">
+        <div className="flex justify-center items-center p-1">
+          <NextUI.Button
+            color="primary"
+            style={{
+              backgroundColor: theme.colors.danger,
+              color: theme.colors.primary,
+            }}
+            className="font-semibold"
+          >
+            Danger
+          </NextUI.Button>
+        </div>
+        <div className="flex justify-center items-center p-1">
+          <NextUI.Button
+            color="primary"
+            style={{
+              backgroundColor: theme.colors.dark,
+              color: theme.colors.primary,
+            }}
+            className="font-semibold"
+          >
+            Dark
+          </NextUI.Button>
+        </div>
+        <div className="flex justify-center items-center p-1">
+          <NextUI.Button
+            color="primary"
+            style={{
+              backgroundColor: theme.colors.secondary,
+              color: theme.colors.primary,
+            }}
+            className="font-semibold"
+          >
+            Secondary
+          </NextUI.Button>
+        </div>
+        <div className="flex justify-center items-center p-1">
+          <NextUI.Button
+            color="primary"
+            style={{
+              backgroundColor: theme.colors.third,
+              color: theme.colors.primary,
+            }}
+            className="font-semibold"
+          >
+            Third
+          </NextUI.Button>
+        </div>
+        <div className="flex justify-center items-center p-1">
+          <NextUI.Button
+            color="primary"
+            style={{
+              backgroundColor: theme.colors.warning,
+              color: theme.colors.primary,
+            }}
+            className="font-semibold"
+          >
+            Warning
+          </NextUI.Button>
+        </div>
+      </div>
     </>
   );
 }
