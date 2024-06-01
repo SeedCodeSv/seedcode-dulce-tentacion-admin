fa5b4f582073c052f2b7abe50bb198a18ff1465f
ROBER-SALAMA <robeetosalama12@gmail.com>
2024-06-01 11:57:51 -0600

    feat: add filter of role in users
    correction in the user page and addead the role filter

--- src/store/users.store.ts
+++ src/store/users.store.ts
@@ -25,9 +25,9 @@ export const useUsersStore = create<UsersStore>((set, get) => ({
   },
   active_filter: 1,
   loading_users: false,
-  async getUsersPaginated(page, limit, userName, active = 1) {
+  async getUsersPaginated(page, limit, userName, role, active = 1) {
     set({ active_filter: active, loading_users: true });
-    await get_user_paginated(page, limit, userName, active)
+    await get_user_paginated(page, limit, userName, role, active)
       .then(({ data }) => {
         set((state) => ({ ...state, users_paginated: data, loading_users: false }));
       })
@@ -59,7 +59,7 @@ export const useUsersStore = create<UsersStore>((set, get) => ({
   postUser(payload) {
     return save_user(payload)
       .then(({ data }) => {
-        get().getUsersPaginated(1, 5, '', get().active_filter);
+        get().getUsersPaginated(1, 5, '', '', get().active_filter);
         toast.success(messages.success);
         return data.ok;
       })
@@ -71,7 +71,7 @@ export const useUsersStore = create<UsersStore>((set, get) => ({
   patchUser(payload, id) {
     return patch_user(payload, id)
       .then((res) => {
-        get().getUsersPaginated(1, 5, '', get().active_filter);
+        get().getUsersPaginated(1, 5, '', '', get().active_filter);
         toast.success(messages.success);
         return res.data.ok;
       })
@@ -83,7 +83,7 @@ export const useUsersStore = create<UsersStore>((set, get) => ({
   deleteUser(id) {
     return delete_user(id)
       .then(({ data }) => {
-        get().getUsersPaginated(1, 5, '', get().active_filter);
+        get().getUsersPaginated(1, 5, '', '', get().active_filter);
         toast.success(messages.success);
         return data.ok;
       })
