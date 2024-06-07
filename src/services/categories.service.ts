857cc5bd82b97770785f26ee4edfb2ab59867fea
ROBER-SALAMA <robeetosalama12@gmail.com>
2024-06-07 10:18:03 -0600

    fix: remove relation
    remove relation of transmiter in category

--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import { IGetCategories, IGetCategoriesPaginated } from '../types/categories.types';
 import { API_URL } from '../utils/constants';
-import { get_token, get_user } from '../storage/localStorage';
+import { get_token } from '../storage/localStorage';
 
 export const get_products_categories = (page = 1, limit = 8, name = '', active = 1) => {
-  const user = get_user();
   const token = get_token() ?? '';
   return axios.get<IGetCategoriesPaginated>(
     API_URL +
-      `/category-products/list-paginated/${user?.employee.branch.transmitterId}?page=${page}&limit=${limit}&name=${name}&active=${active}`,
+      `/category-products/list-paginated?page=${page}&limit=${limit}&name=${name}&active=${active}`,
     {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -19,12 +18,12 @@ export const get_products_categories = (page = 1, limit = 8, name = '', active =
 
 export const create_category = ({ name }: { name: string }) => {
   const token = get_token() ?? '';
-  const user = get_user();
+  // const user = get_user();
   return axios.post<{ ok: boolean }>(
     API_URL + '/category-products',
     {
       name,
-      transmitterId: user?.employee.branch.transmitterId,
+      // transmitterId: user?.employee.branch.transmitterId,
     },
     {
       headers: {
@@ -51,9 +50,8 @@ export const update_category = ({ name }: { name: string }, id: number) => {
 
 export const get_categories = () => {
   const token = get_token() ?? '';
-  const user = get_user();
   return axios.get<IGetCategories>(
-    API_URL + `/category-products/list-by-transmitter/${user?.employee.branch.transmitterId}`,
+    API_URL + `/category-products`,
     {
       headers: {
         Authorization: `Bearer ${token}`,
