f1be8593c6f5b706b5f89dd8f392e321a49daf80
marjorie <alexandramoran9704@gmail.com>
2024-06-24 16:11:57 -0600

    add subcategory type, service, store

---
+++ src/services/sub-category.service.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { get_token } from "../storage/localStorage";
+import { IGetSubCategory } from "../types/sub-category.types";
+import { API_URL } from "../utils/constants";
+
+export const get_subcategories = (id:number) => {
+    const token = get_token() ?? '';
+    return axios.get<IGetSubCategory>(API_URL + `/sub-categories/by-category/:${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
---
+++ src/store/sub-categories.store.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+import { ISubCategoryStore } from "./types/sub_categories_store.types";
+import { get_subcategories } from "../services/sub-category.service";
+
+export const useSubCategoriesStore = create<ISubCategoryStore>((set) => ({
+    subcategories: [],
+    getSubcategories(id) {
+        get_subcategories(id)
+            .then((subcategories) => set({ subcategories: subcategories.data.subcategories }))
+            .catch(() => {
+                set({ subcategories: [] });
+            });
+    },
+}))
