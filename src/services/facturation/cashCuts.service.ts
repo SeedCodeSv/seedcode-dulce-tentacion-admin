ca30fd2265ca7a912075b2f8feda786e5059660e
YeniferCenteno <yenifercentenogr@gmail.com>
2024-06-28 12:03:34 -0600

    corte gran z

---
+++ src/services/facturation/cashCuts.service.ts
@@ -0,0 +1,17 @@
+
+import axios from 'axios'
+import { CloseZ, ZCashCutsRequest } from '../../types/cashCuts.types'
+import { API_URL } from '../../utils/constants'
+export const get_cashCuts = (id?: number, startDate?: string, endDate?: string) => {
+    return axios.get<ZCashCutsRequest>(API_URL + '/sales/z-cash-cuts?' + "branchId=" + id + '&startDate=' + startDate + '&endDate=' + endDate)
+}
+
+
+export const get_cashCuts_x = (id?: number, code?: string) => {
+    return axios.get<ZCashCutsRequest>(API_URL + '/sales/x-cash-cuts?' + "branchId=" + id + '&code=' + code)
+}
+
+export const close_x = (payload: CloseZ) => {
+    return axios.post(API_URL + "/cuts", payload)
+}
+
