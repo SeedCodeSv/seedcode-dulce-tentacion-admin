87a27be925b99d22d3fa8b0465cb16f2564d7b0c
manuel <hernazmeme@gmail.com>
2024-07-11 10:55:45 -0600

    fixed errors

--- src/components/Action_rol/ListActionRol.tsx
+++ src/components/Action_rol/ListActionRol.tsx
@@ -1,5 +1,5 @@
 import { Button, ButtonGroup, useDisclosure } from '@nextui-org/react';
-import { Table as ITable, CreditCard, List} from 'lucide-react';
+import { Table as ITable, CreditCard, List } from 'lucide-react';
 import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../../hooks/useTheme';
 import AddButton from '../global/AddButton';
@@ -14,7 +14,7 @@ const ListActionRol = () => {
   const [view, setView] = useState<'table' | 'grid' | 'list'>('table');
   const [rolId, setRolId] = useState(0);
   const modalAdd = useDisclosure();
-  
+
   const { OnGetActionsRoleList, roleActions } = useActionsRolStore();
   useEffect(() => {
     const getRolId = () => {
@@ -30,6 +30,8 @@ const ListActionRol = () => {
     backgroundColor: theme.colors.dark,
     color: theme.colors.primary,
   };
+  const uniqueRoles = [...new Set(roleActions.map((item) => item.role?.name))];
+
   return (
     <>
       <div className="w-full h-full p-5 bg-gray-50 dark:bg-gray-800">
@@ -37,43 +39,6 @@ const ListActionRol = () => {
           <div className="flex flex-col justify-between w-full gap-5 mb-5 lg:mb-10 lg:flex-row lg:gap-0">
             <div className="flex items-start gap-3"></div>
             <div className="flex w-full">
-              <div className="items-start justify-between w-full gap-10 lg:justify-start">
-                <ButtonGroup>
-                  <Button
-                    isIconOnly
-                    color="secondary"
-                    style={{
-                      backgroundColor: view === 'table' ? theme.colors.third : '#e5e5e5',
-                      color: view === 'table' ? theme.colors.primary : '#3e3e3e',
-                    }}
-                    onClick={() => setView('table')}
-                  >
-                    <ITable />
-                  </Button>
-                  <Button
-                    isIconOnly
-                    color="default"
-                    style={{
-                      backgroundColor: view === 'grid' ? theme.colors.third : '#e5e5e5',
-                      color: view === 'grid' ? theme.colors.primary : '#3e3e3e',
-                    }}
-                    onClick={() => setView('grid')}
-                  >
-                    <CreditCard />
-                  </Button>
-                  <Button
-                    isIconOnly
-                    color="default"
-                    style={{
-                      backgroundColor: view === 'list' ? theme.colors.third : '#e5e5e5',
-                      color: view === 'list' ? theme.colors.primary : '#3e3e3e',
-                    }}
-                    onClick={() => setView('list')}
-                  >
-                    <List />
-                  </Button>
-                </ButtonGroup>
-              </div>
               <div className="flex items-end justify-end">
                 <AddButton
                   onClick={() => {
@@ -83,40 +48,14 @@ const ListActionRol = () => {
               </div>
             </div>
           </div>
-          {view === 'table' && (
-            <DataTable
-              className="w-full shadow "
-              emptyMessage="No se encontraron resultados"
-              value={roleActions}
-              tableStyle={{ minWidth: '50rem' }}
-            >
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={{ ...style, borderTopLeftRadius: '10px' }}
-                field="id"
-                className="text-center justify-center"
-                header="No."
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="role.name"
-                header="Rol"
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="action.view.name"
-                header="Modulo"
-              />
-              <Column
-                headerClassName="text-sm font-semibold"
-                headerStyle={style}
-                field="action.name"
-                header="Acción"
-              />
-            </DataTable>
-          )}
+          <div className="grid dark:bg-slate-800 pb-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-nogutter gap-5 mt-5">
+            {uniqueRoles.map((role) => (
+              <div className="w-full shadow-sm hover:shadow-lg p-8 dark:border dark:border-gray-600 rounded-2xl">
+                <p className="font-semibold dark:text-white">{role}</p>
+                <div className="flex justify-between mt-5 w-full"></div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <HeadlessModal
@@ -132,3 +71,41 @@ const ListActionRol = () => {
 };
 
 export default ListActionRol;
+
+//  <div className="items-start justify-between w-full gap-10 lg:justify-start">
+//    <ButtonGroup>
+//      <Button
+//        isIconOnly
+//        color="secondary"
+//        style={{
+//          backgroundColor: view === 'table' ? theme.colors.third : '#e5e5e5',
+//          color: view === 'table' ? theme.colors.primary : '#3e3e3e',
+//        }}
+//        onClick={() => setView('table')}
+//      >
+//        <ITable />
+//      </Button>
+//      <Button
+//        isIconOnly
+//        color="default"
+//        style={{
+//          backgroundColor: view === 'grid' ? theme.colors.third : '#e5e5e5',
+//          color: view === 'grid' ? theme.colors.primary : '#3e3e3e',
+//        }}
+//        onClick={() => setView('grid')}
+//      >
+//        <CreditCard />
+//      </Button>
+//      <Button
+//        isIconOnly
+//        color="default"
+//        style={{
+//          backgroundColor: view === 'list' ? theme.colors.third : '#e5e5e5',
+//          color: view === 'list' ? theme.colors.primary : '#3e3e3e',
+//        }}
+//        onClick={() => setView('list')}
+//      >
+//        <List />
+//      </Button>
+//    </ButtonGroup>
+//  </div>;
