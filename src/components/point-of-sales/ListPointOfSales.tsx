9a5a10ad3717bb733e5bbda744824a349b7a8e60
manuel <hernazmeme@gmail.com>
2024-09-11 10:57:48 -0600

    permisos and desing in point of sales

--- src/components/point-of-sales/ListPointOfSales.tsx
+++ src/components/point-of-sales/ListPointOfSales.tsx
@@ -218,7 +218,7 @@ function ListPointOfSales({ actions }: Props) {
           </div>
 
           <>
-            <div className="mt-4">
+            {/* <div className="mt-4">
               {loading_point_of_sales_list ? (
                 // Mostrar el componente de carga mientras los datos se están cargando
                 <div className="flex justify-center items-center h-full">
@@ -346,7 +346,132 @@ function ListPointOfSales({ actions }: Props) {
                   <NoData />
                 </div>
               )}
-            </div>
+            </div> */}
+
+<div className="mt-4">
+  {loading_point_of_sales_list ? (
+    // Mostrar el componente de carga mientras los datos se están cargando
+    <div className="flex justify-center items-center h-full">
+      <LoadingTable />
+    </div>
+  ) : point_of_sales_list?.pointOfSales &&
+    Object.keys(point_of_sales_list.pointOfSales).length > 0 ? (
+    Object.entries(point_of_sales_list.pointOfSales).map(
+      ([_locationKey, salePointArray], index) => (
+        <div
+          key={index}
+          className="max-h-[400px] overflow-y-auto overflow-x-auto custom-scrollbar mt-4 mb-20" // Aquí agregamos el margen inferior
+        >
+          <table className="w-full border-collapse border border-gray-200 rounded-lg">
+            <thead>
+              <tr>
+                <th
+                  colSpan={5}
+                  className="p-3 text-center bg-[#1D3557] text-lg font-semibold dark:text-gray-100 dark:bg-slate-700 text-white border border-gray-200 sticky top-0 z-30 rounded-t-lg"
+                >
+                  {point_of_sales_list.name || 'Nombre de la Sucursal'} -
+                  {salePointArray[0]?.code || 'Código'} -
+                  {salePointArray[0]?.codPuntoVenta || 'Código Punto de Venta'}
+                </th>
+              </tr>
+            </thead>
+
+            <thead className="sticky top-[48px] z-20 bg-white border border-gray-200">
+              <tr>
+                <th className="p-3 text-sm font-semibold text-left text-slate-600 dark:text-gray-100 dark:bg-slate-700 bg-slate-200 border border-gray-200">
+                  Tipo
+                </th>
+                <th className="p-3 text-sm font-semibold text-left text-slate-600 dark:text-gray-100 dark:bg-slate-700 bg-slate-200 border border-gray-200">
+                  Descripción
+                </th>
+                <th className="p-3 text-sm font-semibold text-left text-slate-600 dark:text-gray-100 dark:bg-slate-700 bg-slate-200 border border-gray-200">
+                  Anterior
+                </th>
+                <th className="p-3 text-sm font-semibold text-left text-slate-600 dark:text-gray-100 dark:bg-slate-700 bg-slate-200 border border-gray-200">
+                  Siguiente
+                </th>
+                <th className="p-3 text-sm font-semibold text-left text-slate-600 dark:text-gray-100 dark:bg-slate-700 bg-slate-200 border border-gray-200">
+                  Acciones
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="max-h-[600px] w-full overflow-y-auto border border-gray-200">
+              {salePointArray.length === 0 ? (
+                <tr>
+                  <td
+                    colSpan={5}
+                    className="p-3 text-sm text-center text-slate-500 border border-gray-200"
+                  >
+                    No se encontraron registros.
+                  </td>
+                </tr>
+              ) : (
+                salePointArray.map((salePoint: PointOfSales, idx: number) => (
+                  <tr className="border border-gray-400 h-16" key={idx}>
+                    <td className="p-3 text-sm text-slate-500 dark:text-slate-100 whitespace-nowrap border border-gray-200">
+                      {salePoint.typeVoucher}
+                    </td>
+                    <td className="p-3 text-sm text-slate-500 dark:text-slate-100 border border-gray-200">
+                      {salePoint.description}
+                    </td>
+                    <td className="p-3 text-sm text-slate-500 whitespace-nowrap dark:text-slate-100 border border-gray-200">
+                      <input
+                        type="number"
+                        value={
+                          editableSales.find((item) => item.id === salePoint.id)?.prev ?? salePoint.prev
+                        }
+                        onInput={(e) => handleChange(salePoint.id, 'prev', e.currentTarget.value)}
+                        className="border p-2 rounded-md text-sm"
+                        placeholder="Anterior"
+                      />
+                    </td>
+                    <td className="p-3 text-sm text-slate-500 whitespace-nowrap dark:text-slate-100 border border-gray-200">
+                      <input
+                        type="number"
+                        value={
+                          editableSales.find((item) => item.id === salePoint.id)?.next ?? salePoint.next
+                        }
+                        onInput={(e) => handleChange(salePoint.id, 'next', e.currentTarget.value)}
+                        className="border p-2 rounded-md text-sm"
+                        placeholder="Siguiente"
+                      />
+                    </td>
+                    <td className="p-3 text-sm text-slate-500 whitespace-nowrap dark:text-slate-100 border border-gray-200">
+                      {actions.includes('Editar') && (
+                        <TooltipGlobal text="Editar">
+                          <Button
+                            className="border border-white"
+                            onClick={() => {
+                              handleEdit(salePoint);
+                            }}
+                            isIconOnly
+                            style={{ backgroundColor: theme.colors.secondary }}
+                          >
+                            <EditIcon
+                              style={{ color: theme.colors.primary }}
+                              size={20}
+                            />
+                          </Button>
+                        </TooltipGlobal>
+                      )}
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
+      )
+    )
+  ) : (
+    // Mostrar la imagen NoData si no hay datos filtrados
+    <div className="flex justify-center items-center h-full">
+      <NoData />
+    </div>
+  )}
+</div>
+
           </>
         </div>
       </div>
