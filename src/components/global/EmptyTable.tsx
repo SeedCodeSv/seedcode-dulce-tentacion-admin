b397f224910be133447815c7e8e31677caf6165d
jaime221 <jaime7651@gmail.com>
2024-09-11 14:44:45 -0700

    update

--- src/components/global/EmptyTable.tsx
+++ src/components/global/EmptyTable.tsx
@@ -1,18 +1,13 @@
-import NO_DATA from "../../assets/svg/NO_DATA.svg"
+import Lottie from 'lottie-react';
+import NO_DATA from '../../assets/svg/NoSelectProduct.json';
 
 function EmptyTable() {
-    return (
-        <div className="flex flex-col items-center justify-center w-full">
-            <img
-                src={NO_DATA}
-                alt="X"
-                className="w-32 h-32"
-            />
-            <p className="mt-3 text-xl dark:text-white">
-                No se encontraron resultados
-            </p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <Lottie animationData={NO_DATA} className="w-80 h-80 " color="#274c77" />
+      <p className="mt-3 text-xl dark:text-white">No se encontraron resultados</p>
+    </div>
+  );
 }
 
-export default EmptyTable
+export default EmptyTable;
