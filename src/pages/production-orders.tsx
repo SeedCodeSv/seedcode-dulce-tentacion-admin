bf5fe11ef6d30e457e4fcaa3edded36066f50286
marjoriemendoza <alexandramoran9704@gmail.com>
2025-05-27 10:25:14 -0600

    change order production

--- src/pages/production-orders.tsx
+++ src/pages/production-orders.tsx
@@ -332,17 +332,22 @@ function ProductionOrders() {
           <DetailsProductionOrder
             id={selectedOrderId}
             modalMoreInformation={modalMoreInformation}
+            onClose={()=>{
+              setSelectedOrderId(0)
+            }}
           />
         )}
         <VerifyProductionOrder disclosure={modalVerifyOrder} id={selectedOrderId ?? 0} 
         onReload={() => {
           setPage(1)
           getProductionsOrders(page, 5, startDate, endDate, 0, '', 0, 0);
+          setSelectedOrderId(0)
         }} />
         <CompleteOrder disclosure={modalCompleteOrder} id={selectedOrderId ?? 0} 
         reload={() => {
           setPage(1);
           getProductionsOrders(page, 5, startDate, endDate, 0, '', 0, 0);
+          setSelectedOrderId(0)
         }}/>
       </DivGlobal>
     </Layout>
