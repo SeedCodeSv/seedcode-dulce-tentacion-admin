5ade761eaf5fbcb0eafbf05c18bb94259f388012
Henry09Ramos <henryjeremiasramos09@gmail.com>
2025-06-05 13:18:21 -0600

    correction in export report sales by periods

--- src/components/export-reports/types/sales_by_periods.types.ts
+++ src/components/export-reports/types/sales_by_periods.types.ts
@@ -1,10 +1,130 @@
-export interface RespSalesByPeriods {
+// export interface RespSalesByPeriods {
+//   ok: boolean
+//   message: string
+//   sales: SaleByPeriod[]
+// }
+
+// export interface SaleByPeriod {
+//   id: number
+//   numeroControl: string
+//   codigoGeneracion: string
+//   tipoDte: string
+//   typeVoucher: string
+//   fecEmi: string
+//   horEmi: string
+//   selloRecibido: string
+//   selloInvalidacion: string
+//   sello: boolean
+//   codeEmployee: string
+//   totalNoSuj: string
+//   totalExenta: string
+//   totalGravada: string
+//   subTotalVentas: string
+//   descuNoSuj: string
+//   descuExenta: string
+//   descuGravada: string
+//   porcentajeDescuento: string
+//   totalDescu: string
+//   subTotal: string
+//   totalIva: string
+//   montoTotalOperacion: string
+//   totalPagar: string
+//   totalLetras: string
+//   pathPdf: string
+//   pathJson: string
+//   isActivated: boolean
+//   paymentType: string
+//   box: Box
+//   boxId: number
+//   customerId: number
+//   employeeId: number
+//   salesStatusId: number
+// }
+
+// export interface Box {
+//   id: number
+//   start: string
+//   end: string
+//   totalSales: string
+//   totalExpense: string
+//   totalIva: string
+//   date: string
+//   time: string
+//   isActive: boolean
+//   pointOfSale: PointOfSale
+//   pointOfSaleId: number
+// }
+
+// export interface PointOfSale {
+//   id: number
+//   code: string
+//   typeVoucher: string
+//   description: string
+//   resolution: string
+//   serie: string
+//   from: string
+//   to: string
+//   prev: number
+//   next: number
+//   codPuntoVentaMH: any
+//   codPuntoVenta: string
+//   isActive: boolean
+//   branch: Branch
+//   branchId: number
+// }
+
+// export interface Branch {
+//   id: number
+//   name: string
+//   address: string
+//   phone: string
+//   isActive: boolean
+//   codEstableMH: string
+//   codEstable: string
+//   tipoEstablecimiento: string
+//   transmitterId: number
+// }
+
+
+// export interface ResponseDetailsReport {
+//   ok: boolean
+//   sales: SaleDetailsReport[]
+//   total: number
+//   totalSales: number
+//   countSales: number
+//   currentPag: number
+//   nextPag: number
+//   prevPag: number
+//   status: number
+// }
+
+// export interface SaleDetailsReport {
+//   id: number
+//   numeroControl: string
+//   montoTotal: number
+//   fecEmi: string
+//   branch: string
+// }
+
+
+export interface ResponseDetailsReport {
   ok: boolean
-  message: string
-  sales: SaleByPeriod[]
+  sales: SaleDetailsReport[]
+  total: number
+  totalSales: string
+  countSales: number
+  currentPag: number
+  nextPag: number
+  prevPag: number
+  status: number
 }
 
-export interface SaleByPeriod {
+export interface SaleDetailsReport {
+  sale: Sale2
+  branch: string
+}
+
+export interface Sale2 {
   id: number
   numeroControl: string
   codigoGeneracion: string
@@ -35,6 +155,7 @@ export interface SaleByPeriod {
   isActivated: boolean
   paymentType: string
   box: Box
+  payments: Payment[]
   boxId: number
   customerId: number
   employeeId: number
@@ -85,23 +206,12 @@ export interface Branch {
   transmitterId: number
 }
 
-
-export interface ResponseDetailsReport {
-  ok: boolean
-  sales: SaleDetailsReport[]
-  total: number
-  totalSales: number
-  countSales: number
-  currentPag: number
-  nextPag: number
-  prevPag: number
-  status: number
-}
-
-export interface SaleDetailsReport {
+export interface Payment {
   id: number
-  numeroControl: string
-  montoTotal: number
-  fecEmi: string
-  branch: string
+  code: string
+  amount: number
+  reference: string
+  period: any
+  deadline: any
+  saleId: number
 }
